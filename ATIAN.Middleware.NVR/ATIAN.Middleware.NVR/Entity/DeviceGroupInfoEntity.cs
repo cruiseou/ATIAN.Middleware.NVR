cee192cb6ca4f93ba7b269d6592d20e2ffd75455
yangjj@aten-optics.com <221867cf>
2019-10-14 10:34:06 +0800

    新增类

---
+++ ATIAN.Middleware.NVR/ATIAN.Middleware.NVR/Entity/DeviceGroupInfoEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIAN.Middleware.NVR.Entity
+{
+  public  class DeviceGroupInfoEntity
+    {
+        public int GroupID { get; set; }
+
+        public string GroupType { get; set; }
+
+    }
+}
---
+++ ATIAN.Middleware.NVR/ATIAN.Middleware.NVR/Entity/VideoConversionEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIAN.Middleware.NVR.Entity
+{
+    public class VideoConversionEntity : AlarmConvertEntity
+    {
+        public StringBuilder name { get; set; }
+        public DateTime filename { get; set; }
+
+        public string fullpath { get; set; }
+
+        public string NVRSerialNo { get; set; }
+
+        public int NVRChannelNo { get; set; }
+        public string DeviceName { get; set; }
+    }
+}
